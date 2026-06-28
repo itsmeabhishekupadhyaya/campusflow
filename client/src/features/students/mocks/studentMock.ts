b480b8f1464ab20cce3d5010bde1fbbd9f84c64a
Abhishek Upadhyaya <abhishek.upadhyaya23@gmail.com>
2026-06-28 23:21:17 +0530

    feat(student): implement student management foundation

--- client/src/features/students/mocks/studentMock.ts
+++ client/src/features/students/mocks/studentMock.ts
@@ -2,6 +2,6 @@ import type { Student } from '../types/student';
 
 export const mockStudents: Student[] = [
   { id: 1, firstName: 'Abhishek', lastName: 'Kumar' },
-  { id: 2, firstName: 'Abhishek', lastName: 'Upadhyaya' },
-  { id: 3, firstName: 'Abhishek', lastName: 'Kumar' },
+  { id: 2, firstName: 'Sumit', lastName: 'Upadhyaya' },
+  { id: 3, firstName: 'Deepak', lastName: 'Kumar' },
 ];
